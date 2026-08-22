285e454768879239beba5ff3d2da023e06d183f3
moheyy28@gamil.com <moheyy28@gmail.com>
2026-08-22 18:32:48 +0300

    3622 && try js in 3622

---
+++ 3622. Check Divisibility by Digit Sum and Product.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var checkDivisibility = function (n) {
+    summation = 0 
+    
+};
+
+
+/*
+/**
+ * @param {number} n
+ * @return {boolean}
+ *
+var checkDivisibility = function (n) {
+    digit_product = 1
+    digit_sum = 0
+
+    for (i of n.toString()) {
+        num = Number(i)
+        digit_sum += num
+        digit_product *= num
+    }
+
+    sum = digit_product + digit_sum
+
+    return n % sum == 0
+};
+ */
