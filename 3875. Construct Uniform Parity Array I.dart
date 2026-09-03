328001d2d04871ac98781dac59b0a0327dd37b9c
moheyy28@gamil.com <moheyy28@gmail.com>
2026-09-03 15:37:59 +0300

    3876

--- 3875. Construct Uniform Parity Array I.dart
+++
@@ -1,8 +0,0 @@
-class Solution {
-  bool uniformArray(List<int> nums1) => true;
-}
-
-void main(List<String> args) {
-  print(Solution().uniformArray([2, 3])); // true
-  print(Solution().uniformArray([4, 6])); // true
-}
